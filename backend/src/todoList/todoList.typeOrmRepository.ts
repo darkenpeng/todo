f5f5b2ceeb2791770ce01f52fa5d358abdc99ebd
chloeseo0210 <chloeseo0210@gmail.com>
2022-07-24 22:32:05 +0900

    typeORMRepository 수정

--- backend/src/todoList/todoList.typeOrmRepository.ts
+++ backend/src/todoList/todoList.typeOrmRepository.ts
@@ -1,49 +1,28 @@
-import { Entity , Column, PrimaryColumn, DataSource, Repository } from "typeorm"
-import { Injectable } from "@nestjs/common";
-
-
-@Entity("Todos")
-export class TodoTable {
-    @PrimaryColumn('text', { nullable: false, length: 140, name: "Content" })
-    content: string
-    
-    @Column('boolean',{ nullable: false, name: "Completed" })
-    completed: boolean
-
-    @Column('integer', { nullable: false, name: "CreatedAt"})
-    createdAt: number
-}
-
-const AppDataSource = new DataSource({
-     type : "sqlite",
-    database: "database.db",
-    synchronize: true,
-    logging: true,
-    entities: [TodoTable],
-    subscribers: [],
-    migrations: [],
-}).initialize()
+import { Entity , Column, PrimaryColumn, Repository, DataSource } from "typeorm"
+import { Inject, Injectable } from "@nestjs/common";
+import { TodoTable } from "./todoList.todoTable";
 
 @Injectable()
 export class TypeOrmRepository implements ITodoRepository {
-  _repository: Promise<Repository<TodoTable>>
-  constructor() {
-    this._repository = AppDataSource.then(dataSource => dataSource.getRepository(TodoTable))
+  _repository: Repository<TodoTable>
+  constructor(
+    @Inject("DATA_SOURCE")
+    dataSource: DataSource
+  ) {
+    this._repository = dataSource.getRepository(TodoTable);
   }
 
   async saveAll(newTodoList: Todo[]) {
-      const repository = await this._repository;
       const oldTodoList = await this.getAll();
       // delete 구현해야 
       // newTodoList에서 oldTodo의 content와 같은 게 한개도 없다면~ 
       const deletedTodos = oldTodoList.filter((oldTodo)=> newTodoList.every((newTodo)=> newTodo.content !== oldTodo.content))
-      await repository.remove(deletedTodos)
+      await this._repository.remove(deletedTodos)
 
-      await repository.save(newTodoList) // insert, update
+      await this._repository.save(newTodoList) // insert, update
   }
 
   async getAll() {
-    const repository = await this._repository;
-    return repository.find()
+    return this._repository.find()
   }
 }
